26389786eb961f091b7309343de3da1a7e76fc09
wenta <jakubwenta@gmail.com>
2017-11-22 13:32:34 +0100

    Fix for data-path

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar /root/borsuk.jar --kafka=$KAFKA_BROKER --statsd-host=$STATSD_HOST --statsd-host=$DATA_PATH
+java -jar /root/borsuk.jar --kafka=$KAFKA_BROKER --statsd-host=$STATSD_HOST --data-path=$DATA_PATH
 
