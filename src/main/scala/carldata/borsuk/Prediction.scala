3f861ddc7be7caa678b06526c7b035a3a8e4d1e8
wenta <jakubwenta@gmail.com>
2018-02-20 13:40:54 +0100

    Use daily pattern from timeseries library in predict function. Fixed #16

--- src/main/scala/carldata/borsuk/Prediction.scala
+++ src/main/scala/carldata/borsuk/Prediction.scala
@@ -1,8 +1,10 @@
 package carldata.borsuk
 
 import java.time._
+import java.time.temporal.{ChronoUnit, TemporalUnit}
 
-import carldata.series.{Gen, TimeSeries}
+import carldata.series.{Patterns, TimeSeries}
+
 
 object Prediction {
   def fit(ts: TimeSeries[Double]): Prediction = {
@@ -12,12 +14,33 @@ object Prediction {
 
 class Prediction(ts: TimeSeries[Double]) {
   def predict(day: LocalDate): TimeSeries[Double] = {
-    val idx = (for (i <- 0 to 287) yield
-      LocalDateTime.of(day, LocalTime.MIDNIGHT)
-        .plusSeconds(60 * 60 * i)
-        .toInstant(ZoneOffset.UTC)).toVector
+    val instDay = LocalDateTime.of(day, LocalTime.MIDNIGHT).toInstant(ZoneOffset.UTC)
+    val duration = Duration.ofMinutes(5)
 
-    Gen.randomNoise(idx, 0.5, 0.7)
+    def f(x1: (Instant, Double), x2: (Instant, Double), tsh: Instant): Double = {
+      val tx = Duration.between(tsh, x1._1).toMillis
+      val ty = Duration.between(tsh, x2._1).toMillis
+      (ty / (tx + ty) * x1._2) + (tx / (tx + ty) * x2._2)
+    }
+
+    def g(vs: TimeSeries[Double]): TimeSeries[Double] = {
+      if (vs.idx.last.isAfter(instDay)) {
+        vs.filter(x => LocalDateTime.ofInstant(x._1, ZoneOffset.UTC).toLocalDate.equals(day))
+      }
+      else {
+        val xs = Patterns.daily(vs)
+        val idx = xs.idx.map { x =>
+          LocalDateTime.ofInstant(vs.take(vs.length - 3).idx.last, ZoneOffset.UTC)
+            .truncatedTo(ChronoUnit.DAYS)
+            .plusSeconds(x.getEpochSecond + 86400)
+            .toInstant(ZoneOffset.UTC)
+        }
+        g(new TimeSeries(vs.idx ++ idx, vs.values ++ xs.values))
+      }
+    }
+
+    val xs = TimeSeries.interpolate(ts, duration).addMissing(duration, f)
+    g(xs)
   }
 
 }
