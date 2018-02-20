b79ea7e4632667dcc57dbab8ad312651dcec3557
wenta <jakubwenta@gmail.com>
2018-02-20 16:30:44 +0100

    Fix after code review.

--- src/main/scala/carldata/borsuk/Prediction.scala
+++ src/main/scala/carldata/borsuk/Prediction.scala
@@ -1,20 +1,12 @@
 package carldata.borsuk
 
 import java.time._
-import java.time.temporal.{ChronoUnit, TemporalUnit}
 
 import carldata.series.{Patterns, TimeSeries}
 
 
 object Prediction {
   def fit(ts: TimeSeries[Double]): Prediction = {
-    new Prediction(ts)
-  }
-}
-
-class Prediction(ts: TimeSeries[Double]) {
-  def predict(day: LocalDate): TimeSeries[Double] = {
-    val instDay = LocalDateTime.of(day, LocalTime.MIDNIGHT).toInstant(ZoneOffset.UTC)
     val duration = Duration.ofMinutes(5)
 
     def f(x1: (Instant, Double), x2: (Instant, Double), tsh: Instant): Double = {
@@ -23,24 +15,20 @@ class Prediction(ts: TimeSeries[Double]) {
       (ty / (tx + ty) * x1._2) + (tx / (tx + ty) * x2._2)
     }
 
-    def g(vs: TimeSeries[Double]): TimeSeries[Double] = {
-      if (vs.idx.last.isAfter(instDay)) {
-        vs.filter(x => LocalDateTime.ofInstant(x._1, ZoneOffset.UTC).toLocalDate.equals(day))
-      }
-      else {
-        val xs = Patterns.daily(vs)
-        val idx = xs.idx.map { x =>
-          LocalDateTime.ofInstant(vs.take(vs.length - 3).idx.last, ZoneOffset.UTC)
-            .truncatedTo(ChronoUnit.DAYS)
-            .plusSeconds(x.getEpochSecond + 86400)
-            .toInstant(ZoneOffset.UTC)
-        }
-        g(new TimeSeries(vs.idx ++ idx, vs.values ++ xs.values))
-      }
-    }
-
     val xs = TimeSeries.interpolate(ts, duration).addMissing(duration, f)
-    g(xs)
+    val xs2 = Patterns.daily(xs)
+    new Prediction(xs2)
+  }
+}
+
+class Prediction(ts: TimeSeries[Double]) {
+  def predict(day: LocalDate): TimeSeries[Double] = {
+    val idx = ts.idx.map { x =>
+      LocalDateTime.of(day, LocalTime.MIDNIGHT)
+        .plusSeconds(x.getEpochSecond)
+        .toInstant(ZoneOffset.UTC)
+    }
+    new TimeSeries[Double](idx, ts.values)
   }
 
 }
