d21d5c6c5a5c64cdfb9dec13d7fcfd142cbc5da8
wenta <jakubwenta@gmail.com>
2018-08-09 11:03:50 +0200

    Refactor REST API. Fixed #26

--- src/main/scala/carldata/borsuk/ApiRoutes.scala
+++ src/main/scala/carldata/borsuk/ApiRoutes.scala
@@ -1,13 +1,15 @@
 package carldata.borsuk
 
-import java.time.{Instant, LocalDate}
+import java.time.Instant
+import java.util.UUID.randomUUID
 
-import akka.http.scaladsl.model.HttpMethods
+import akka.http.scaladsl.model.{HttpMethods, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Route, StandardRoute}
 import carldata.series.{Csv, TimeSeries}
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
 import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
+import spray.json._
 
 import scala.collection.immutable.Seq
 import scala.language.postfixOps
@@ -15,7 +17,7 @@ import scala.language.postfixOps
 case class Parameters(project: String, flow: String, projectsUrl: String)
 
 class ApiRoutes(storage: Storage) {
-
+  val models = collection.mutable.Map.empty[String, String]
   val settings: CorsSettings.Default = CorsSettings.defaultSettings.copy(allowedMethods = Seq(
     HttpMethods.GET,
     HttpMethods.POST,
@@ -23,31 +25,76 @@ class ApiRoutes(storage: Storage) {
     HttpMethods.HEAD,
     HttpMethods.OPTIONS))
 
+
   /** Routing */
   def route(): Route = cors(settings) {
     path("api" / "healthcheck") {
       complete("Ok")
-    } ~ (path("api" / "prediction" / Remaining) & parameters("flow".as[String], "rain" ?, "day".as[String])) {
-      (project, flow, rain, day) =>
-        get {
-          val flowTs = storage.getTimeSeries(project, flow)
-          val rainTs = if (rain.isDefined) storage.getTimeSeries(project, rain.get) else TimeSeries.empty[Double]
-          predict(day, flowTs, rainTs)
+    } ~ path("api" / "model") {
+      post {
+        entity(as[String]) {
+          body =>
+            if (body.isEmpty) complete("type:error\nEmpty body.")
+            else {
+              val json = body.parseJson.asJsObject
+              if (json.fields.contains("type")) {
+                val id = randomUUID().toString
+                models += id -> "props"
+
+                complete("{\"id\": \"" + id + "\"}")
+              }
+              else complete("type:error\nModel type not provided.")
+            }
         }
-    } ~ (path("api" / "anomaly" / Remaining) & parameters("series".as[String])) {
-      (project, series) =>
-        get {
-          val ts = storage.getTimeSeries(project, series)
-          anomaly(ts)
+      }
+    } ~ path("api" / "model" / Segment / "fit") {
+      id =>
+        post {
+          entity(as[String]) {
+            body =>
+              if (models.contains(id)) {
+                if (body.isEmpty) complete("type:error\nEmpty body.")
+                else {
+                  val json = body.parseJson.asJsObject
+                  if (json.fields.contains("features") && json.fields.contains("labels")) {
+                    complete(StatusCodes.OK)
+                  }
+                  else complete("type:error\nFeatures or labels not provided.")
+                }
+              }
+              else complete(StatusCodes.NotFound)
+          }
+        }
+    } ~ path("api" / "model" / Segment / "predict") {
+      id =>
+        post {
+          entity(as[String]) {
+            body =>
+              if (models.contains(id)) {
+                if (body.isEmpty) complete("type:error\nEmpty body.")
+                else {
+                  val json = body.parseJson.asJsObject
+                  if (json.fields.contains("features")) {
+                    complete("{\n  \"labels\": [1,2,3]\n}")
+                  }
+                  else complete("type:error\nFeatures not provided.")
+
+                }
+              }
+              else complete(StatusCodes.NotFound)
+          }
+        }
+    } ~ path("api" / "model" / Segment / "status") {
+      id =>
+        post {
+          if (models.contains(id)) {
+            complete("{\n  \"build\": \"1\"\n}")
+          }
+          else complete(StatusCodes.NotFound)
         }
     }
   }
 
-  def predict(day: String, flow: TimeSeries[Double], rain: TimeSeries[Double]): StandardRoute = {
-    val partialFlow = flow.slice(flow.index.head, dateParse(day))
-    val prediction = Prediction.fit(partialFlow, rain).predict(LocalDate.parse(day))
-    complete(Csv.toCsv(prediction))
-  }
 
   def anomaly(ts: TimeSeries[Double]): StandardRoute = {
     val cleanedTs = Anomaly.fixAnomalies(ts)
