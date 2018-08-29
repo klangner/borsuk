ef3b558abbb19331fa400cfb9ea09223b2841a15
Krzysztof Langner <klangner@gmail.com>
2018-08-29 10:00:24 +0200

    Prediction based on decision tree

--- src/main/scala/carldata/borsuk/prediction/Prediction.scala
+++ src/main/scala/carldata/borsuk/prediction/Prediction.scala
@@ -2,20 +2,27 @@ package carldata.borsuk.prediction
 
 import java.util.UUID.randomUUID
 
+import smile.regression.{RandomForest, randomForest}
 
+
+/** Prediction for the time series data */
 class Prediction(modelType: String) {
 
   val id: String = randomUUID().toString
+  var model: Option[RandomForest] = None
   var buildNumber: Int = 0
   var score: Double = 0.0
 
   /** Fit model */
-  def fit(data: Array[Double]): Unit = {
+  def fit(ts: Array[Double]): Unit = {
+    val features: Array[Array[Double]] = ts.indices.map(_ % 24).map(i => Array(i.toDouble)).toArray
+    model = Some(randomForest(features, ts, mtry=1))
     buildNumber += 1
   }
 
   /** Predict values for the next 24h */
   def predict(): Array[Double] = {
-    0.to(24).map(_.toDouble).toArray
+    val features = 0.to(24).map(i => Array(i.toDouble)).toArray
+    model.map(_.predict(features)).getOrElse(Array())
   }
 }
