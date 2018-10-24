1299a34e27473695e3c6ca913f5a23ac8d651785
Tomasz Pichlak <tpichlak@gmail.com>
2018-10-24 10:19:01 +0200

    Test for different DryWeatherPattern search.

--- src/test/scala/borsuk/DryWeatherPatternTest.scala
+++ src/test/scala/borsuk/DryWeatherPatternTest.scala
@@ -12,22 +12,24 @@ class DryWeatherPatternTest extends WordSpec with Matchers {
     "find dry weather day before rain started" in {
 
       val day = LocalDate.of(2018, 1, 1) //Monday
-      val allDwpDays = Seq(LocalDate.of(2017, 12, 31), //Sunday
-        LocalDate.of(2017, 12, 30), //Saturday
-        LocalDate.of(2017, 12, 29), //Friday
-        LocalDate.of(2017, 12, 28), //Thursday
-        LocalDate.of(2017, 12, 27), //Wednesday
-        LocalDate.of(2017, 12, 26), //Tuesday
+      val allDwpDays = Seq(
         LocalDate.of(2017, 12, 25), //Monday
+        LocalDate.of(2017, 12, 26), //Tuesday
+        LocalDate.of(2017, 12, 27), //Wednesday
+        LocalDate.of(2017, 12, 28), //Thursday
+        LocalDate.of(2017, 12, 29), //Friday
+        LocalDate.of(2017, 12, 30), //Saturday
+        LocalDate.of(2017, 12, 31), //Sunday
       )
       val dayWithDryDayPattern = DryWeatherPattern.findDryDay2(day, allDwpDays)
-      dayWithDryDayPattern.get shouldBe LocalDate.of(2017, 12, 25)
+      dayWithDryDayPattern.get shouldBe LocalDate.of(2017, 12, 29)
     }
 
     "find dry weather day after rain started" in {
 
       val day = LocalDate.of(2018, 1, 1) // Monday
-      val allDwpDays = Seq(LocalDate.of(2018, 1, 8), //Monday
+      val allDwpDays = Seq(
+        LocalDate.of(2018, 1, 8), //Monday
         LocalDate.of(2018, 1, 9), //Tuesday
         LocalDate.of(2018, 1, 10), //Wednesday
         LocalDate.of(2018, 1, 11), //Thursday
