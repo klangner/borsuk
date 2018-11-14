0949df6747491e0c12cebec1db518d6ab72e47a7
Tomasz Pichlak <tpichlak@gmail.com>
2018-11-14 12:08:20 +0100

    Added dummy test file for serialization. Both tests are failing.

---
+++ src/test/scala/borsuk/EnvelopeSerializationTest.scala
@@ -0,0 +1,19 @@
+package borsuk
+
+import org.scalatest.{WordSpec, Matchers}
+
+class EnvelopeSerializationTest extends WordSpec with Matchers {
+
+  "EnvelopeResult" should {
+
+    "serialize and deserialize back to proper EnvelopeResult" in {
+      0 shouldEqual 1
+    }
+
+    "should be the same after serialization and deserialization of HashMap" in {
+      0 shouldEqual 1
+    }
+
+  }
+
+}
