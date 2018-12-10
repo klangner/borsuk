598242f45b39efaa9ace873b12f8c2736b9ce935
mjaniakcds <35447404+mjaniakcds@users.noreply.github.com>
2018-12-10 15:57:59 +0100

    test 1

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -Dlogback.configurationFile=/root/gelf.xml  -jar /root/borsuk.jar --data-url=$DATA_URL -XX:MaxRAM=3892m -Xmx3892m -Xss3500m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC
+java -Dlogback.configurationFile=/root/gelf.xml  -jar /root/borsuk.jar --data-url=$DATA_URL -Xmx3892m
 
