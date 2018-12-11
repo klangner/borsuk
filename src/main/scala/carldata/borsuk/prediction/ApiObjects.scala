8dd5bc29d918c67c112814cc7d902ae0042f6b27
wenta <jakubwenta@gmail.com>
2018-12-11 12:17:40 +0100

    Extend prediction to 7 seven days. Fixed #175

--- src/main/scala/carldata/borsuk/prediction/ApiObjects.scala
+++ src/main/scala/carldata/borsuk/prediction/ApiObjects.scala
@@ -20,9 +20,9 @@ object ApiObjects {
 
   case class FitPredictionParams(flow: TimeSeriesParams, rainfall: TimeSeriesParams)
 
-  case class PredictionRequest(startDate: LocalDateTime, samples: Int)
+  case class PredictionRequest(startDate: LocalDateTime)
 
-  case class PredictionResponse(values: Array[Double])
+  case class PredictionResponse(flow: TimeSeriesParams)
 
   case class ModelStatus(build: Int, score: Double)
 
@@ -122,17 +122,15 @@ object ApiObjectsJsonProtocol extends DefaultJsonProtocol {
   implicit object PredictionRequestFormat extends RootJsonFormat[PredictionRequest] {
     def write(req: PredictionRequest): JsObject = {
       JsObject(
-        "start-date" -> JsString(req.startDate.toString),
-        "samples" -> JsNumber(req.samples)
+        "start-date" -> JsString(req.startDate.toString)
       )
     }
 
     def read(value: JsValue): PredictionRequest = value match {
       case JsObject(request) =>
         val startDate = request.get("start-date").map(timestampFromValue).getOrElse(LocalDateTime.now())
-        val samples = request.get("samples").map(doubleFromValue).map(_.toInt).getOrElse(0)
-        PredictionRequest(startDate, samples)
-      case _ => PredictionRequest(LocalDateTime.now(), 0)
+        PredictionRequest(startDate)
+      case _ => PredictionRequest(LocalDateTime.now())
     }
   }
 
@@ -140,17 +138,18 @@ object ApiObjectsJsonProtocol extends DefaultJsonProtocol {
     * PredictionResponse formatter
     */
   implicit object PredictionResponseFormat extends RootJsonFormat[PredictionResponse] {
+    val emptyTSP = TimeSeriesParams(LocalDateTime.now(), Duration.ZERO, Array())
     def write(res: PredictionResponse): JsObject = {
       JsObject(
-        "values" -> JsArray(res.values.map(JsNumber(_)).toVector)
+        "flow" -> res.flow.toJson
       )
     }
 
     def read(value: JsValue): PredictionResponse = value match {
       case JsObject(request) =>
-        val values = request.get("values").map(arrayFromValue(_, doubleFromValue)).getOrElse(Array.empty[Double])
-        PredictionResponse(values)
-      case _ => PredictionResponse(Array.empty)
+        val flow = request.get("flow").map(_.convertTo[TimeSeriesParams]).getOrElse(emptyTSP)
+        PredictionResponse(flow)
+      case _ => PredictionResponse(emptyTSP)
     }
   }
 
--- src/main/scala/carldata/borsuk/prediction/Prediction.scala
+++ src/main/scala/carldata/borsuk/prediction/Prediction.scala
@@ -1,11 +1,14 @@
 package carldata.borsuk.prediction
 
-import java.time.LocalDate
+import java.time.{Duration, LocalDate, LocalDateTime}
 
+import carldata.borsuk.BasicApiObjects.TimeSeriesParams
+import carldata.borsuk.helper.TimeSeriesHelper
 import carldata.series.TimeSeries
 
 /** Prediction for the time series data */
 class Prediction(modelType: String, id: String) {
+  private val emptyTSP = TimeSeriesParams(LocalDateTime.now(), Duration.ZERO, Array())
   var model: Option[trainedModel] = None
   var buildNumber: Int = 0
   var score: Double = 0.0
@@ -19,13 +22,16 @@ class Prediction(modelType: String, id: String) {
     }
   }
 
-  /** Predict values for the next 24h */
-  def predict(day: LocalDate): Array[Double] = {
-    model.map {
-      DailyPatternModel.predict(day, _)
-        .features
-        .values.unzip._1.toArray
-    }
-      .getOrElse(Array())
+  /** Predict values for the next 7 days */
+  def predict(startDay: LocalDate): TimeSeriesParams = {
+    model.map { m: trainedModel =>
+      val xs = (for (i: Int <- 0 until 7) yield i)
+        .map { day =>
+          DailyPatternModel.predict(startDay.plusDays(day), m)
+            .features
+        }
+      TimeSeriesHelper.toTimeSeriesParams(TimeSeriesHelper.concat(xs).mapValues(_._1))
+    }.getOrElse(emptyTSP)
   }
+
 }
